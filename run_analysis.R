cf5de37c284665949d3258560b206f6e478e160c
tadkar <tadkar@yahoo.com>
2014-04-28 01:18:05 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 library(plyr)
 library(reshape2)
 
-X_test <- read.table("~/GettingAndCleaningData/X_test.txt", quote="\"",row.names=NULL)
-X_train <- read.table("~/GettingAndCleaningData/X_train.txt", quote="\"",row.names=NULL)
-y_test <- read.table("~/GettingAndCleaningData/y_test.txt", quote="\"",row.names=NULL)
-y_train <- read.table("~/GettingAndCleaningData/y_train.txt", quote="\"",row.names=NULL)
-subject_test <- read.table("~/GettingAndCleaningData/subject_test.txt", quote="\"",row.names=NULL)
-subject_train <- read.table("~/GettingAndCleaningData/subject_train.txt", quote="\"",row.names=NULL)
+X_test <- read.table("~/X_test.txt", quote="\"",row.names=NULL)
+X_train <- read.table("~/X_train.txt", quote="\"",row.names=NULL)
+y_test <- read.table("~/y_test.txt", quote="\"",row.names=NULL)
+y_train <- read.table("~/y_train.txt", quote="\"",row.names=NULL)
+subject_test <- read.table("~/subject_test.txt", quote="\"",row.names=NULL)
+subject_train <- read.table("~/subject_train.txt", quote="\"",row.names=NULL)
 
 names(y_test)<-"Activity"
 names(y_train)<-"Activity"
